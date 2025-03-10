bd03e2fa5027834ebb9cd282539afbeed53e2dd8
Sergio <d.chile58@live.fr>
2025-03-10 16:54:03 +0100

    test: Ajout des premiers tests unitaires

---
+++ test/notes.test.js
@@ -0,0 +1,82 @@
+const assert = require('assert')
+
+const {addNote,getAllNotes,getNoteById,updateNote, deleteNote} = require('../functions/notes')
+
+
+describe('Notes - Unitaire', () => {
+    describe('Ajoute une note à la liste actuelle', () => {
+        it('retourne la note ajoutée', () => {
+            let result = addNote('mes devoirs', 'math : exo 2; francais : dictée')
+            assert.equal(result.title, 'mes devoirs')
+            assert.equal(result.content, 'math : exo 2; francais : dictée')
+
+            result = addNote('prochain rdv', 'le 3 mars à 10h')
+            assert.equal(result.title, 'prochain rdv')
+            assert.equal(result.content, 'le 3 mars à 10h')
+        })
+    })
+
+    describe('Retourne la liste de notes', () => {
+        it('Doit retourner la liste de note actuelle au moement de l\'appel', () => {
+            let result = getAllNotes()
+            assert.equal(result[0].title, 'mes devoirs')
+            assert.equal(result[0].content, 'math : exo 2; francais : dictée')
+
+            assert.equal(result[1].title, 'prochain rdv')
+            assert.equal(result[1].content, 'le 3 mars à 10h')    
+        })
+    })
+
+    describe('Retourne une note à partir de son id', () => {
+        it('Doit retourner la note', () => {
+            let result = getNoteById(1)
+            assert.equal(result.title, 'mes devoirs')
+            assert.equal(result.content, 'math : exo 2; francais : dictée')
+
+            result = getNoteById(2)
+            assert.equal(result.title, 'prochain rdv')
+            assert.equal(result.content, 'le 3 mars à 10h')   
+        })
+
+        it('Doit retourner null', () => {
+            let result = getNoteById(3)
+            assert.equal(result, null)
+
+            result = getNoteById(10)
+            assert.equal(result, null)
+        })
+    })
+
+    describe('Supprime une note à partir de son id', () => {
+        it('Doit retourner la note supprimée', () => {
+            let result = deleteNote(1)
+            assert.equal(result.title, 'mes devoirs')
+            assert.equal(result.content, 'math : exo 2; francais : dictée') 
+
+            result = deleteNote(2)
+            assert.equal(result.title, 'prochain rdv')
+            assert.equal(result.content, 'le 3 mars à 10h')   
+        })
+
+        it('Doit retourner null', () => {
+            let result = getNoteById(1)
+            assert.equal(result, null)
+
+            result = getNoteById(2)
+            assert.equal(result, null)
+        })
+    })
+})
+
+describe('Notes - Intégration', () => {
+    describe('Retourne la liste de notes', () => {
+        it('Doit retourner la liste de note actuelle au moement de l\'appel', () => {
+            let result = getAllNotes()
+            assert.equal(result, [])
+
+            addNote('prochain rdv', 'le 3 mars à 10h')
+            result = getAllNotes()
+            assert.equal(result[0], {id: 1, title: 'prochain rdv', content: 'le 3 mars à 10h'})
+        })
+    })
+})
